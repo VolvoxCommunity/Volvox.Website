d55c2e3bafe3c75e9559267637613de1360caa7a
Bill Chirico <bill@chirico.dev>
2025-12-13 02:03:30 -0500

    refactor(product-screenshots): use filename as stable key

--- src/components/products/product-screenshots.tsx
+++ src/components/products/product-screenshots.tsx
@@ -44,7 +44,7 @@ export function ProductScreenshots({
             const imagePath = `/images/product/${slug}/${screenshot}`;
             return (
               <motion.button
-                key={idx}
+                key={screenshot}
                 initial={{ opacity: 0, scale: 0.95 }}
                 whileInView={{ opacity: 1, scale: 1 }}
                 viewport={{ once: true }}
