8e56623bce166f44c0f34bbf09e9012438658c67
Bill Chirico <bill@chirico.dev>
2026-01-20 11:10:53 -0500

    refactor: clean up next.config.ts by removing unnecessary host entries
    - Removed unused host entries for i.pravatar.cc and i.ibb.co.com from the next.config.ts file to streamline configuration.

--- next.config.ts
+++ next.config.ts
@@ -8,14 +8,6 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "github.com",
       },
-      {
-        protocol: "https",
-        hostname: "i.pravatar.cc",
-      },
-      {
-        protocol: "https",
-        hostname: "i.ibb.co.com",
-      },
     ],
   },
   headers() {
